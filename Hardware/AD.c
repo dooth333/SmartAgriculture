fe3a5cf071f80d915c1becf99deeb1fcc0bad667
wenc <2729420403@qq.com>
2024-05-25 07:59:28 +0800

    Add AD.c and AD.h files, add ADC conversion function(添加文件AD.c和AD.h，增加ADC转换功能)

---
+++ Hardware/AD.c
@@ -0,0 +1,51 @@
+#include "stm32f10x.h"                  // Device header
+#include "AD.h"   
+uint16_t AD_Value[3];//用于存储光照强度（0）和土壤湿度（1）
+uint16_t Light,soilHumidity;
+
+/**
+  * @brief  ADC初始化和DMA初始化（ADC转换出三个模拟值后通过DMA转运到AD_Value数组中）
+  * @param  无
+  * @retval None
+  */
+void AD_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	
+	RCC_ADCCLKConfig(RCC_PCLK2_Div6); 
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7; 
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_7, 1, ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 2, ADC_SampleTime_55Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_9, 3, ADC_SampleTime_55Cycles5);
+		
+	ADC_InitTypeDef ADC_InitStructure;
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+	ADC_InitStructure.ADC_NbrOfChannel = 3;
+	ADC_Init(ADC1, &ADC_InitStructure);
+	
+	
+	ADC_ResetCalibration(ADC1);
+	while (ADC_GetResetCalibrationStatus(ADC1) == SET);
+	ADC_StartCalibration(ADC1);
+	while (ADC_GetCalibrationStatus(ADC1) == SET);
+	
+	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+}
---
+++ Hardware/AD.h
@@ -0,0 +1,8 @@
+#ifndef __AD_H
+#define __AD_H
+
+extern uint16_t AD_Value[3];
+extern uint16_t Light,soilHumidity;
+void AD_Init(void);
+
+#endif
