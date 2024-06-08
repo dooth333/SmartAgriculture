5fef5e87d9141ff3bea5eeba91f562aec243a708
wenc <2729420403@qq.com>
2024-06-08 23:01:55 +0800

    Modify key driver, key initialization（修改按键驱动，按键初始化）

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -1,34 +1,50 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h" // Device header
+#include "stdio.h"
+#include <string.h>
 #include "Delay.h"
+// 硬件驱动
+#include "Key.h"
+#include "LED.h"
+#include "usart.h"
 
+#include "OLED.h"
+
+struct Keys_Struct Keys[4];
+
+char text[30];
+
+struct KEY_MSG key_msg = {0, false};
+
+/**
+ * @brief  按键初始化，按键一端接单片机引脚，一端接GND
+ * @param  None
+ * @retval None
+ */
 void Key_Init(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	for (uint8_t j = 0; j < 4; j++)
+	{
+		Keys[j].Step = 0;
+
+		Keys[j].Key_Single_Flag = 0;
+
+		Keys[j].Key_Double_Flag = 0;
+
+		Keys[j].Key_Long_Flag = 0;
+
+		Keys[j].Time_Count_Flag = 0;
+
+		Keys[j].Press_Time_Count = 0;
+	}
 }
 
-uint8_t Key_GetNum(void)
-{
-	uint8_t KeyNum = 0;
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
-	{
-		Delay_ms(20);
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0);
-		Delay_ms(20);
-		KeyNum = 1;
-	}
-	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0)
-	{
-		Delay_ms(20);
-		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11) == 0);
-		Delay_ms(20);
-		KeyNum = 2;
-	}
-	
-	return KeyNum;
-}
+
+
+
